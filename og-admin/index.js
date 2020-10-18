16c7d41c6317999335b5a13539c8fc44c45e9279
omegagames.helpersomegagames5 <omegagames.helpers@gmail.com>
2020-10-18 14:09:37 +0400

    ogadmin0_0

---
+++ og-admin/index.js
@@ -0,0 +1,41 @@
+var logOnCode = "x68D-XxcF-8X9e-9uYe-oPeT-9U6T";
+
+function logOn() {
+
+var el0 = document.getElementById("lgntxbx0");
+
+var el1 = document.getElementById("status");
+
+var el2 = document.getElementById("lgnbtn0");
+
+var el0i = document.querySelector('.lgntxbx0i');
+
+if ( el0i.value == logOnCode) {
+
+console.log("(INFO) Logging in...");
+
+window.location.assign("https://omegagames5.github.io/og-admin/x68D-XxcF-8X9e-9uYe-oPeT-9U6T.html");
+
+
+	
+}
+
+else {
+
+el1.innerHTML = "Wrong LogIn Code!";
+
+el2.enabled = false;
+
+console.log("(Error) Unable to verify login code!");
+
+setTimeout(()=>{
+
+el1.innerHTML = "";
+
+el2.enabled = true;
+
+}, 1850);
+	
+}
+
+}
